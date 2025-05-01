12de15323465da95a57c20b8c692f970d37847fb
Vianney-conception <vianney.gervo.fr>
2025-05-01 23:54:02 +0200

    mise en place des btn

--- header/index.php
+++ header/index.php
@@ -1,1 +1,5 @@
-<header></header>
+<header>
+    <a href="http://localhost/Acceuil/"><img class="img-btn" src="../img/LogoN&B.svg" alt=""></a>
+    <a class="btn" href="http://localhost/Acceuil">Acceuil</a>
+    <a class="btn" href="http://localhost/Contact">Contact</a>
+</header>
