0e06d886186770de7f7e2d2867f9f4b0ed1935a1
Vianney-conception <vianney.gervo.fr>
2025-05-02 16:53:16 +0200

    Mise en place CV

--- Accueil/index.php
+++ Accueil/index.php
@@ -25,8 +25,8 @@
 
     <div style="justify-content: space-between;" class="containerClassique">
         <h2>CV :</h2>
-        <a class="btn" href="/CV/">
-        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg> Voire le CV</a>
+        <a class="btn" href="/CV/" target="_blank">
+        <svg style="margin-right: 5px;" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg> Voire le CV</a>
 
     </div>
 
