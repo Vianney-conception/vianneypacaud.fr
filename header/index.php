60a35af793e08d7efc743b9beb2deb999e8b9d97
Vianney-conception <vianney.gervo.fr>
2025-05-02 12:19:29 +0200

    Mise en place de btn

--- header/index.php
+++ header/index.php
@@ -1,5 +1,16 @@
 <header>
-    <a href="http://localhost/Acceuil/"><img class="img-btn" src="../img/LogoN&B.svg" alt=""></a>
-    <a class="btn" href="http://localhost/Acceuil">Acceuil</a>
-    <a class="btn" href="http://localhost/Contact">Contact</a>
+  <div class="logo-container">
+    <a href="/Acceuil/">
+      <img class="img-btn" src="../img/LogoN&B.svg" alt="Logo Vianney Pacaud">
+    </a>
+    <p class="TitreLogo">Vianney Pacaud</p>
+  </div>
+  <nav class="btn-container">
+    <a class="btn pageActuel" href="/Acceuil/">Accueil</a>
+    <a class="btn" href="/Service/">Services</a>
+    <a class="btn" href="/Contact/">Contact</a>
+  </nav>
+  <nav class="btn-Menu">
+  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+  </nav>
 </header>
