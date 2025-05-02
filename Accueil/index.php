874e051831085126b36d05cdc8d414ae84df45af
Vianney-conception <vianney.gervo.fr>
2025-05-02 16:50:57 +0200

    Mise en place CV

--- Accueil/index.php
+++ Accueil/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,15 +8,30 @@
     <link rel="stylesheet" href="../css/styles.css">
     <link rel="icon" href="../img/Logo_N&B.png" type="image/x-icon">
 </head>
+
 <body>
     <?php include '../header/index.php'; ?>
 
     <div class="containerClassique">
-        <img src="../img/Vianney_PacaudT.webp" alt="">
+        <img class="Portrait" src="../img/Vianney_PacaudT.webp" alt="">
         <div class="text">
-
+            <div class="citation">
+                <h1>Bonjour,</h1>
+                <p>Je m'appelle Vianney, étant passionné par l'informatique et tout particulièrement par le développement web, je propose de vous accompagner tout au long de votre projet. Ne vous inquiétez plus, je m'occupe de tout ! De votre site Web aux services de messagerie, vous pouvez compter sur moi </p>
+            </div>
         </div>
+    </div>
+    </div>
+
+    <div style="justify-content: space-between;" class="containerClassique">
+        <h2>CV :</h2>
+        <a class="btn" href="/CV/">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFFFFF"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg> Voire le CV</a>
 
     </div>
+
+
+
 </body>
+
 </html>
