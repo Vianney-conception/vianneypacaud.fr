fff58edd633e8d653b9f934eac1f7e89932d980e
Vianney-conception <vianney.gervo.fr>
2025-05-16 21:33:21 +0200

    Corection ortographe

--- footer/index.php
+++ footer/index.php
@@ -1,5 +1,5 @@
 <footer class="containerClassique">
-  <p>Dérniére mise à jour : 02/05/2025</p>
+  <p>Dérnière mise à jour : 02/05/2025</p>
   <p>Ce site est en cours de développement, certaines fonctionnalités peuvent être manquantes.</p>
   <p>Ce site n'utilise pas de cookies et ne préléve aucune information sur les visiteurs</p>
   <p>© 2025 Vianney Pacaud. Tous droits réservés.</p>
