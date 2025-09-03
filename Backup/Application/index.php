9b06710799cb185114dca79228c0e1885598ee91
Vianney PACAUD <vianney@gervo.fr>
2025-09-03 21:56:43 +0200

    Nouvelle Base

---
+++ Backup/Application/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vianney Pacaud - Accueil</title>
+    <link rel="stylesheet" href="../css/styles.css">
+    <?php include '../facivon/index.php'; ?>
+</head>
+
+<body class="Claire">
+    <?php include '../header/index.php'; ?>
+
+
+    
+
+
+
+  
+    <?php include '../footer/index.php'; ?>
+
+</body>
+<script src="/Scripts/MoteurRendu.js"></script>
+<script src="/Scripts/Langages.js"></script>
+<script src="/Scripts/cookie.js"></script>
+
+</html>
